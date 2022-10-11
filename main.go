313e1adb850e7f4931055781d40176fe81766115
Manish Kumar <manish.kumar@Manish-k-MacBook-Air.local>
2022-10-12 01:30:05 +0530

    added remote GET

--- main.go
+++ main.go
@@ -5,9 +5,11 @@ import (
 	"net/http"
 	"strconv"
 	"github.com/syndtr/goleveldb/leveldb"
+	"flag"
+	"strings"
 )
 
-type gocache struct {
+type GoCache struct {
 	db *leveldb.DB
 	
 	// params
@@ -15,6 +17,7 @@ type gocache struct {
 	replicas int
 	md5sum bool
 	volumes []string
+	db_path string
 }
 
 
@@ -22,13 +25,80 @@ func getRoot(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "Server is Running...")
 }
 
+func getPingPong(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "Pong")
+}
+
+func  (clnt *GoCache) GetRecord(key []byte) string {
+	data, err := clnt.db.Get(key, nil)
+	//rec := Record{[]string{}, HARD, ""}
+	if err != leveldb.ErrNotFound {
+	//	rec = toRecord(data)
+	}
+	return string(data)
+}
+
+
+
+func (clnt *GoCache) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	key := []byte(r.URL.Path)
+	log_key := string(key[1:])
+	new_key := []byte(log_key)
+
+
+	fmt.Println("GET_key ", log_key, new_key)
+
+	if r.Method == "GET" {
+		rec := clnt.GetRecord(key)
+		io.WriteString(w, rec)
+
+	}
+	
+}
+
+
+
+
 func main() {
-	PORT := 3699
-	fmt.Println("GoCache is Running on Port:", PORT)
+	fmt.Println("GoCache is Running...")
 	fmt.Println("============================")
 	
-	http.HandleFunc("/", getRoot)
-	err := http.ListenAndServe(":"+strconv.Itoa(PORT), nil)
+	port := flag.Int("port", 3699, "Port to serve")
+	db_path := flag.String("db_path", "", "Path to key-value db may levelDb")
+	replicas := flag.Int("replicas", 3, "Amount of replicas")
+	pvolumes := flag.String("volumes", "", "voulmes to use to store , comma seprated")
+	md5sum := flag.Bool("md5sum", true, "calculate and store md5 checksum")
+	volumes := strings.Split(*pvolumes, ",")
+	flag.Parse()
+
+	_a, _b, _c , _d, _e := port, db_path, replicas, volumes, md5sum
+	fmt.Println("cmdline params ", *_a, *_b,*_c, _d, *_e)
+	fmt.Printf("Server running at 0:%d\n",*port)
+	
+
+	db, db_err := leveldb.OpenFile(*db_path, nil)
+	if db_err != nil {
+		panic(fmt.Sprintf("LevelDB open failed %s", db_err))
+	}
+	defer  db.Close()  // close db later
+
+	put_err := db.Put([]byte("manish"), []byte("17bcs2423"), nil)
+	fmt.Println("pur error" , put_err)
+	//http.HandleFunc("/", HttpHandler)
+	http.HandleFunc("/ping", getPingPong)
+	//http.HandleFunc("/get", GETfunc)
+
+
+	client := GoCache{
+			db: db,
+			port: *port,
+			db_path: *db_path,
+			replicas: *replicas,
+			volumes: volumes,
+			md5sum: *md5sum,
+	}
+
+	err := http.ListenAndServe(":"+strconv.Itoa(*port), &client)
 	if err != nil{
 		fmt.Println("Unexpected error occured.")
 	}
